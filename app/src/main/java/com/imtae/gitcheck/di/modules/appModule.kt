d509f7c33e107c65c1d34d42c837479992a916e1
Im-Tae <dlaxo2420@naver.com>
2020-04-13 11:33:55 +0900

    Debugging appModule ProgressUtil context

--- app/src/main/java/com/imtae/gitcheck/di/modules/appModule.kt
+++ app/src/main/java/com/imtae/gitcheck/di/modules/appModule.kt
@@ -1,12 +1,13 @@
 package com.imtae.gitcheck.di.modules
 
+import android.content.Context
 import com.imtae.gitcheck.utils.PreferenceManager
 import com.imtae.gitcheck.utils.ProgressUtil
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
 val appModule = module {
-    factory { ProgressUtil(androidContext()) }
+    factory { (context: Context) -> ProgressUtil(context) }
 
     single { PreferenceManager(androidContext()) }
 }
