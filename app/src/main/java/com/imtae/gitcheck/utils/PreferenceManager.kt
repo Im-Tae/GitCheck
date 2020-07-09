6354d2a5f7008b0a9126a16569b4700948163b51
Im-Tae <dlaxo2420@naver.com>
2020-07-09 11:11:16 +0900

    「UPDATE」 SharedPreferences 암호화

--- app/src/main/java/com/imtae/gitcheck/utils/PreferenceManager.kt
+++ app/src/main/java/com/imtae/gitcheck/utils/PreferenceManager.kt
@@ -3,7 +3,11 @@
 package com.imtae.gitcheck.utils
 
 import android.content.Context
+import android.content.SharedPreferences
 import android.preference.PreferenceManager
+import androidx.security.crypto.EncryptedSharedPreferences
+import androidx.security.crypto.MasterKeys
+import com.google.gson.Gson
 import com.google.gson.GsonBuilder
 import com.imtae.gitcheck.retrofit.data.Key
 import com.imtae.gitcheck.retrofit.domain.User
@@ -11,13 +15,24 @@ import com.imtae.gitcheck.retrofit.domain.User
 
 class PreferenceManager(context : Context) {
 
-    private val pref = PreferenceManager.getDefaultSharedPreferences(context)
+    private val pref: SharedPreferences by lazy {
+        val keyGenParameterSpec = MasterKeys.AES256_GCM_SPEC
+        val masterKeyAlias = MasterKeys.getOrCreate(keyGenParameterSpec)
+
+        EncryptedSharedPreferences.create(
+            "GitCheck",
+            masterKeyAlias,
+            context,
+            EncryptedSharedPreferences.PrefKeyEncryptionScheme.AES256_SIV,
+            EncryptedSharedPreferences.PrefValueEncryptionScheme.AES256_GCM
+        )
+    }
 
     fun getData(key: String): String? = pref.getString(key, Key.NULL.toString())
 
     fun setData(key : String, value : String) = pref.edit().putString(key, value).commit()
 
-    fun getUserInfo(key: String) : User = GsonBuilder().create().fromJson(pref.getString(key, null), User::class.java)
+    fun getUserInfo(key: String) : User = GsonBuilder().create().fromJson(pref.getString(key, Gson().toJson(User())), User::class.java)
 
     fun setUserInfo(key : String, user : User) = pref.edit().putString(key, GsonBuilder().create().toJson(user, User::class.java)).commit()
 
