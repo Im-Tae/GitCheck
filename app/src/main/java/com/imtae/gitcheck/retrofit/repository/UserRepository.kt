481eb4130f6f6fc9b862e1d3fc82d059fa60b388
Im-Tae <dlaxo2420@naver.com>
2020-08-10 14:58:25 +0900

    「UPDATE」 UserRepository getUserInfo 네트워크 미 연결시 계속 불러오기 설정

--- app/src/main/java/com/imtae/gitcheck/retrofit/repository/UserRepository.kt
+++ app/src/main/java/com/imtae/gitcheck/retrofit/repository/UserRepository.kt
@@ -1,20 +1,35 @@
 package com.imtae.gitcheck.retrofit.repository
-import com.imtae.gitcheck.retrofit.data.Key
+
 import com.imtae.gitcheck.retrofit.domain.User
 import com.imtae.gitcheck.retrofit.network.UserApi
+import com.imtae.gitcheck.utils.NetworkUtil
+import io.reactivex.Flowable
 import io.reactivex.Single
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.schedulers.Schedulers
 import org.koin.core.KoinComponent
 import org.koin.core.inject
+import org.koin.core.parameter.parametersOf
 import org.koin.core.qualifier.named
+import java.util.concurrent.TimeUnit
 
 class UserRepository : KoinComponent {
 
     private val userApi : UserApi by inject(named("UserApi"))
 
+    private val networkStatus: NetworkUtil by inject { parametersOf(this) }
+
     fun getUserInfo(token: String): Single<User> =
         userApi.getUserInfo(token)
             .observeOn(AndroidSchedulers.mainThread())
             .subscribeOn(Schedulers.io())
+            .retryWhen {
+                Flowable.interval(3, TimeUnit.SECONDS)
+                    .retryUntil {
+                        if(networkStatus.networkInfo())
+                            return@retryUntil true
+
+                        false
+                    }
+            }
 }
